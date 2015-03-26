058f1534f4e9e2c24c37f0acb18af3c92446352e
Chris Done <chrisdone@gmail.com>
2015-03-26 16:26:15 +0100

    Running Ace example

--- examples/Main.hs
+++ examples/Main.hs
@@ -6,27 +6,35 @@
 
 module Main where
 
+import Data.Maybe
 import React
 import React.Ace
+import React.Builder
 import React.Internal
+import React.Ref
 
 -- | Application state.
 data State =
-  State
+  State { stateAce :: Maybe Ace }
   deriving (Show,Eq)
 
 -- | Main entry point.
 main :: IO ()
 main =
-  do app <- makeApp State id
+  do app <- makeApp (State Nothing) id
      ace <- createAce app
      container <- getElementById "container"
      react app (render ace) container
 
 -- | Our main view.
 render :: Monad m
-       => Component s Ace m -- ^ Ace component.
-       -> s                 -- ^ State.
-       -> ReactT s m ()
+       => Component State Ace m -- ^ Ace component.
+       -> State                 -- ^ State.
+       -> ReactT State m ()
 render ace state =
-  build "div" (text "Hello, world!?")
+  do build "div" (text "Hello, world!?")
+     buildComponent
+       ace
+       (Lens (\(State ace) -> fromJust ace)
+             (\ace state -> State (Just ace)))
+       (attr "code" "Hello, world!")
